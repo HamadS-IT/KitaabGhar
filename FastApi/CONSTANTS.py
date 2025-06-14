646d0574a5113e4e5d0a35d2eee49025153350e9
HamadS. <hamadsaqib2@gmail.com>
2025-06-15 04:15:16 +0500

    Delete FastApi directory

--- FastApi/CONSTANTS.py
+++
@@ -1,9 +0,0 @@
-
-with open(r"stopwords.txt","r",encoding='utf-8') as R:
-    stopwords = [i.replace("\n","") for i in R.readlines()]
-
-
-punctuations = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@'
-             , '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
-
-
